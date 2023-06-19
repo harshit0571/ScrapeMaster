97686ed6a2a4fbd2927e1fae2b3a455514c82e90
harshit0571 <harshitchadha65@gmail.com>
2023-06-19 12:18:45 +0530

    scrapping text

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ console.log("hello");
 async function start() {
   const browser = await puppeteer.launch(); //launches a browser
   const page = await browser.newPage(); //creates a new page in brower
-  await page.goto("https://en.wikipedia.org/wiki/JavaScript"); // page visits site
-  await page.screenshot({ path: "ss.jpeg" }); //takes screenshot
+
+  //screenshot
+  // await page.goto("https://en.wikipedia.org/wiki/JavaScript"); // page visits site
+  // await page.screenshot({ path: "fullpage.jpeg", fullPage: true }); //takes screenshot
+
+  //web scrapping
+  await page.goto("https://learnwebcode.github.io/practice-requests/");
+  const names = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".info strong")).map(
+      (x) => x.textContent
+    );
+  });
+  console.log(names);
+
   await browser.close(); //closes browser
 }
 
