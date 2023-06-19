1d47ad49f0b831d4d6b3b430205275a77a9adca2
harshit0571 <harshitchadha65@gmail.com>
2023-06-19 12:33:45 +0530

    practice done created amazon scrapper file

--- practice.js
+++ practice.js
@@ -10,14 +10,21 @@ async function start() {
   // await page.goto("https://en.wikipedia.org/wiki/JavaScript"); // page visits site
   // await page.screenshot({ path: "fullpage.jpeg", fullPage: true }); //takes screenshot
 
-  //web scrapping
+  // //web scrapping text only
+  // await page.goto("https://learnwebcode.github.io/practice-requests/");
+  // const names = await page.evaluate(() => {
+  //   return Array.from(document.querySelectorAll(".info strong")).map(
+  //     (x) => x.textContent
+  //   );
+  // });
+  // console.log(names);
+
+  //store photos
   await page.goto("https://learnwebcode.github.io/practice-requests/");
-  const names = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll(".info strong")).map(
-      (x) => x.textContent
-    );
+  const photos = await page.$$eval("img", (imgs) => {
+    return imgs.map((x) => x.src);
   });
-  console.log(names);
+  console.log(photos);
 
   await browser.close(); //closes browser
 }
