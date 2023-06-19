093b78e3f74a43855b780e51ccc773abe5cc0b3e
harshit0571 <harshitchadha65@gmail.com>
2023-06-19 14:15:34 +0530

    error in image

--- amazon.js
+++ amazon.js
@@ -10,14 +10,30 @@ async function start() {
   await page.click(".nav-search-submit");
   await page.waitForNavigation();
   await page.screenshot({ path: "assets/2.png", fullPage: true });
-  await page.waitForSelector(".s-image");
+  await page.waitForSelector(".s-result-item");
 
   await page.screenshot({ path: "assets/3.png", fullPage: true });
 
-  const text = await page.$$eval(".s-image", (texts) => {
-    return texts.map((x) => x.src);
-  });
-  console.log(text);
+  const text = await page.$$eval(
+    ".s-result-item  .s-card-border",
+    (container) => {
+      const image = container.map((x) => {
+        x.querySelector("img");
+      });
+      const price = container.map((x) => {
+        x.querySelector(".a-offscreen");
+      });
+      const title = container.map((x) => {
+        x.querySelector("h2 > span");
+      });
+
+      // const imag = image.map((x) => x.src);
+      // price = price.map((x) => x.textContent);
+      // title = title.map((x) => x.textContent);
+      return container;
+    }
+  );
+  console.log(text[0]);
   await browser.close();
 }
 
