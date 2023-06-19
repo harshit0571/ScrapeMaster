cdb8863581c539b4f752e26db8fb9974bd416939
harshit0571 <harshitchadha65@gmail.com>
2023-06-19 23:34:46 +0530

    executablepath changed

--- express.js
+++ express.js
@@ -22,7 +22,14 @@ app.listen(port, ()=>{
 
 
 async function start(search) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({  args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath: usr/bin/google-chrome-stable
+});
   const page = await browser.newPage();
   await page.goto("https://www.amazon.in");
 
