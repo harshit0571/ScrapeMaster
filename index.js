e2e34abaf1226fa0cc3a444f8c83d2f3eed573e7
harshit0571 <harshitchadha65@gmail.com>
2023-06-19 11:50:02 +0530

    basic done screenshot taken

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import puppeteer from "puppeteer";
 
 console.log("hello");
+
+async function start() {
+  const browser = await puppeteer.launch(); //launches a browser
+  const page = await browser.newPage(); //creates a new page in brower
+  await page.goto("https://en.wikipedia.org/wiki/JavaScript"); // page visits site
+  await page.screenshot({ path: "ss.jpeg" }); //takes screenshot
+  await browser.close(); //closes browser
+}
+
+start();
